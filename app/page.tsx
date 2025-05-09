67500222e10b13ca68f368f37eaffadcde3e4c45
jambrizz <ambrizjovani@gmail.com>
2025-05-08 19:10:52 -0700

    New version of starter code

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+﻿//import Image from "next/image";
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="text-4xl font-bold">Hello, Portfolio</h1>
+            <p className="mt-4 text-lg">This is a simple portfolio that is a work in progress.</p>
+        </main>
+  );
+}
